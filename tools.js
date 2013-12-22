b2001c5ac076bbad99979a6bb81be89cda55c4d0
Nicolas McCurdy <thenickperson@gmail.com>
2013-12-22 17:11:38 -0500

    Rename roll to result

--- tools.js
+++ tools.js
@@ -17,51 +17,51 @@ var Tools = {
 
   die: function () {
     View.mode = "die";
-    var roll = Math.floor(Math.random() * 6 + 1);
-    View.display("<img src='images/dice/die_" + roll + " .png' alt='" + roll + " ' height='100' class='number'></img>");
+    var result = Math.floor(Math.random() * 6 + 1);
+    View.display("<img src='images/dice/die_" + result + " .png' alt='" + result + " ' height='100' class='number'></img>");
   },
 
   coin: function () {
     View.mode = "coin";
-    var roll = Math.floor(Math.random() * 2 + 1);
-    if (roll === 1) {
+    var result = Math.floor(Math.random() * 2 + 1);
+    if (result === 1) {
       View.display("<img src='images/coins/coin_heads.png' alt='heads' height='100' class='text'></img>");
     }
-    if (roll === 2) {
+    if (result === 2) {
       View.display("<img src='images/coins/coin_tails.png' alt='tails' height='100' class='text'></img>");
     }
   },
 
   card: function () {
     View.mode = "card";
-    var roll = Math.floor(Math.random() * 54 + 1);
-    if (roll >= 1 && roll <= 13) {
-      View.display("<img src='images/cards/d" + roll + " .png' alt='" + roll + "  of diamonds' height='100' class='text'></img>");
+    var result = Math.floor(Math.random() * 54 + 1);
+    if (result >= 1 && result <= 13) {
+      View.display("<img src='images/cards/d" + result + " .png' alt='" + result + "  of diamonds' height='100' class='text'></img>");
     }
-    if (roll >= 14 && roll <= 26) {
-      View.display("<img src='images/cards/h" + (roll - 13) + " .png' alt='" + (roll - 13) + "  of hearts' height='100' class='text'></img>");
+    if (result >= 14 && result <= 26) {
+      View.display("<img src='images/cards/h" + (result - 13) + " .png' alt='" + (result - 13) + "  of hearts' height='100' class='text'></img>");
     }
-    if (roll >= 27 && roll <= 39) {
-      View.display("<img src='images/cards/s" + (roll - 26) + " .png' alt='" + (roll - 26) + "  of spades' height='100' class='text'></img>");
+    if (result >= 27 && result <= 39) {
+      View.display("<img src='images/cards/s" + (result - 26) + " .png' alt='" + (result - 26) + "  of spades' height='100' class='text'></img>");
     }
-    if (roll >= 40 && roll <= 52) {
-      View.display("<img src='images/cards/c" + (roll - 39) + " .png' alt='" + (roll - 39) + "  of clubs' height='100' class='text'></img>");
+    if (result >= 40 && result <= 52) {
+      View.display("<img src='images/cards/c" + (result - 39) + " .png' alt='" + (result - 39) + "  of clubs' height='100' class='text'></img>");
     }
-    if (roll === 53) {
+    if (result === 53) {
       View.display("<img src='images/cards/jb.png' alt='black joker' height='100' class='text'></img>");
     }
-    if (roll === 54) {
+    if (result === 54) {
       View.display("<img src='images/cards/jr.png' alt='red joker' height='100' class='text'></img>");
     }
   },
 
   number: function () { //BUGGY
     View.mode = "number";
-    var minimum, maximum, roll;
+    var minimum, maximum, result;
     minimum = parseInt($("#minimum").val(), 10) || 1;
     maximum = parseInt($("#maximum").val(), 10) || 10;
-    roll = Math.floor(Math.random() * maximum + minimum);
-    View.display("<span class='number'>" + roll + " </span><br><span class='mute'>from " + minimum + "  to " + maximum + "  </span>");
+    result = Math.floor(Math.random() * maximum + minimum);
+    View.display("<span class='number'>" + result + " </span><br><span class='mute'>from " + minimum + "  to " + maximum + "  </span>");
   },
 
   from_list: function () {
