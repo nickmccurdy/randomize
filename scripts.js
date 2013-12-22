46ed0e7cf15df832db68d6d57866807e475722b0
Nicolas McCurdy <thenickperson@gmail.com>
2013-12-21 23:11:47 -0500

    Fix jslint issues with whitespace

--- scripts.js
+++ scripts.js
@@ -9,37 +9,34 @@ var minimum = 1;
 var maximum = 10;
 
 function display(results) {
-  if($(".results").is(":visible")) {
-    $(".results").fadeOut(function() {
+  if ($(".results").is(":visible")) {
+    $(".results").fadeOut(function () {
       $(".results").html(results);
     });
     $(".results").fadeIn();
     /*
-    if() { //this is for shit that needs to fade in or out when you're switching modes
-      $(".options").slideUp(function() {});
+    if () { //this is for shit that needs to fade in or out when you're switching modes
+      $(".options").slideUp(function () {});
       $(".options").slideDown();
     }
     */
-  }
-  else {
-    $("#welcome").slideUp(function() {
+  } else {
+    $("#welcome").slideUp(function () {
       $(".results").html(results);
       $(".results, .reload-button, .options").slideDown();
     });
   }
-  if(mode==="from_list" || mode==="sort_list") {
+  if (mode === "from_list" || mode === "sort_list") {
     $(".options-header").slideDown();
-    $(".number-options").slideUp(function() {
+    $(".number-options").slideUp(function () {
       $(".list-options").slideDown();
     });
-  }
-  else if(mode==="number") {
+  } else if (mode === "number") {
     $(".options-header").slideDown();
-    $(".list-options").slideUp(function() {
+    $(".list-options").slideUp(function () {
       $(".number-options").slideDown();
     });
-  }
-  else {
+  } else {
     $(".list-options, .number-options, .options-header").slideUp();
   }
 }
@@ -61,25 +58,25 @@ function preload() {
   to_preload.push("images/coins/coin_heads.png");
   to_preload.push("images/coins/coin_tails.png");
   // dice
-  for(i=1; i<=6; i++) {to_preload.push("images/dice/die_"+i+".png");}
+  for (i = 1; i <= 6; i++) { to_preload.push("images/dice/die_" + i + ".png"); }
   // cards
-  for(i=1; i<=13; i++) {to_preload.push("images/cards/c"+i+".png");}
-  for(i=1; i<=13; i++) {to_preload.push("images/cards/d"+i+".png");}
-  for(i=1; i<=13; i++) {to_preload.push("images/cards/h"+i+".png");}
-  for(i=1; i<=13; i++) {to_preload.push("images/cards/s"+i+".png");}
+  for (i = 1; i <= 13; i++) { to_preload.push("images/cards/c" + i + ".png"); }
+  for (i = 1; i <= 13; i++) { to_preload.push("images/cards/d" + i + ".png"); }
+  for (i = 1; i <= 13; i++) { to_preload.push("images/cards/h" + i + ".png"); }
+  for (i = 1; i <= 13; i++) { to_preload.push("images/cards/s" + i + ".png"); }
   to_preload.push("images/cards/jb.png");
   to_preload.push("images/cards/jr.png");
-  
+
   // load stuff!
   var preload_html = "";
   var preload_size = to_preload.length;
-  for(i=0; i<preload_size; i++) {
-    preload_html += "<img src='"+to_preload.shift()+"'>";
+  for (i = 0; i < preload_size; i++) {
+    preload_html += "<img src='" + to_preload.shift() + "'>";
   }
   $("<div id='preloader'></div>").appendTo("body").hide().html(preload_html);
 }
 
-$(function($) {
+$(function ($) {
   // buttons
   $("#dice-button").click(Tools.die);
   $("#coins-button").click(Tools.coin);
@@ -88,7 +85,7 @@ $(function($) {
   $("#from-list-button").click(Tools.from_list);
   $("#sort-list-button").click(Tools.sort_list);
   $(".reload-button").click(reload);
-  
+
   // preload
   preload();
 });
--- tools.js
+++ tools.js
@@ -4,101 +4,96 @@ var Tools = {
 
   die: function () {
     switchMode("die");
-    var roll = Math.floor(Math.random()*6+1);
-    display("<img src='images/dice/die_"+roll+".png' alt='"+roll+"' height='100' class='number'></img>");
+    var roll = Math.floor(Math.random() * 6 + 1);
+    display("<img src='images/dice/die_" + roll + " .png' alt='" + roll + " ' height='100' class='number'></img>");
   },
 
   coin: function () {
     switchMode("coin");
-    var roll = Math.floor(Math.random()*2+1);
-    if(roll===1) {
+    var roll = Math.floor(Math.random() * 2 + 1);
+    if (roll === 1) {
       display("<img src='images/coins/coin_heads.png' alt='heads' height='100' class='text'></img>");
     }
-    if(roll===2) {
+    if (roll === 2) {
       display("<img src='images/coins/coin_tails.png' alt='tails' height='100' class='text'></img>");
     }
   },
 
   card: function () {
     switchMode("card");
-    var roll = Math.floor(Math.random()*54+1);
-    if(roll>=1 && roll<=13) {
-      display("<img src='images/cards/d"+roll+".png' alt='"+roll+" of diamonds' height='100' class='text'></img>");
+    var roll = Math.floor(Math.random() * 54 + 1);
+    if (roll >= 1 && roll <= 13) {
+      display("<img src='images/cards/d" + roll + " .png' alt='" + roll + "  of diamonds' height='100' class='text'></img>");
     }
-    if(roll>=14 && roll<=26) {
-      display("<img src='images/cards/h"+(roll-13)+".png' alt='"+(roll-13)+" of hearts' height='100' class='text'></img>");
+    if (roll >= 14 && roll <= 26) {
+      display("<img src='images/cards/h" + (roll - 13) + " .png' alt='" + (roll - 13) + "  of hearts' height='100' class='text'></img>");
     }
-    if(roll>=27 && roll<=39) {
-      display("<img src='images/cards/s"+(roll-26)+".png' alt='"+(roll-26)+" of spades' height='100' class='text'></img>");
+    if (roll >= 27 && roll <= 39) {
+      display("<img src='images/cards/s" + (roll - 26) + " .png' alt='" + (roll - 26) + "  of spades' height='100' class='text'></img>");
     }
-    if(roll>=40 && roll<=52) {
-      display("<img src='images/cards/c"+(roll-39)+".png' alt='"+(roll-39)+" of clubs' height='100' class='text'></img>");
+    if (roll >= 40 && roll <= 52) {
+      display("<img src='images/cards/c" + (roll - 39) + " .png' alt='" + (roll - 39) + "  of clubs' height='100' class='text'></img>");
     }
-    if(roll===53) {
+    if (roll === 53) {
       display("<img src='images/cards/jb.png' alt='black joker' height='100' class='text'></img>");
     }
-    if(roll===54) {
+    if (roll === 54) {
       display("<img src='images/cards/jr.png' alt='red joker' height='100' class='text'></img>");
     }
   },
 
   number: function () { //BUGGY
     switchMode("number");
-    if(parseInt($("#minimum").val(), 10) && parseInt($("#maximum").val(), 10)) {
+    if (parseInt($("#minimum").val(), 10) && parseInt($("#maximum").val(), 10)) {
       minimum = parseInt($("#minimum").val(), 10);
       maximum = parseInt($("#maximum").val(), 10);
-    }
-    else {
+    } else {
       minimum = 1;
       maximum = 10;
     }
-    var roll = Math.floor(Math.random()*maximum+minimum);
-    display("<span class='number'>"+roll+"</span><br><span class='mute'>from "+minimum+" to "+maximum+" </span>");
+    var roll = Math.floor(Math.random() * maximum + minimum);
+    display("<span class='number'>" + roll + " </span><br><span class='mute'>from " + minimum + "  to " + maximum + "  </span>");
 
   },
 
-  from_list: function ()
-  {
+  from_list: function () {
     var result;
-    if($(".list-options textarea").val()) {
+    if ($(".list-options textarea").val()) {
       result = $(".list-options textarea").val();
-    }
-    else {
+    } else {
       result = "list is empty";
     }
     switchMode("from_list");
     var text_array = [];
     text_array = result.split("\n");
-    text_array = text_array.sort(function() {
+    text_array = text_array.sort(function () {
       return 0.5 - Math.random();
     });
     result = text_array[0];
     /*
     var quantity = 2;
     for(var i=0;i<quantity;i++) {
-      result = result+"<br>"+text_array[i+1];
+      result = result + " <br>" + text_array[i+1];
     }
     */
-    display("<span class='text'>"+result+"</span>");
+    display("<span class='text'>" + result + " </span>");
   },
 
-  sort_list: function ()
-  {
+  sort_list: function () {
     var result;
-    if($(".list-options textarea").val()) {
+    if ($(".list-options textarea").val()) {
       result = $(".list-options textarea").val();
-    }
-    else {
+    } else {
       result = "list is empty";
     }
     switchMode("sort_list");
     var text_array = [];
     text_array = result.split("\n");
-    text_array = text_array.sort(function() {
+    text_array = text_array.sort(function () {
       return 0.5 - Math.random();
     });
     result = text_array.join("</li><li>");
-    display("<span class='list'><ul><li>"+result+"</li></ul></span>");
+    display("<span class='list'><ul><li>" + result + " </li></ul></span>");
   }
 
 };
